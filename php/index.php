6a1385ddef1d281f42e18c74616bb0de3f97539c
Vatsa <sharad.vatsa@te.com>
2019-02-24 11:30:53 +0530

    App connected to DB : 1) PHP app  container connected to MySQL DB 2) Refresh on a browser inserts a dummy record in to database

--- php/index.php
+++ php/index.php
@@ -19,6 +19,52 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully\r\n";
+
+// Create database
+$sql = "CREATE DATABASE IF NOT EXISTS  myDB";
+if ($conn->query($sql) === TRUE) {
+    /* echo "Database created successfully";*/
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+// Create Table
+// sql to create table
+$sql = "CREATE TABLE IF NOT EXISTS myDB.MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if (mysqli_query($conn, $sql)) {
+    /* echo "Table MyGuests created successfully";*/
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+echo "\r\n\n";
+$sql = "INSERT INTO myDB.MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+if ($conn->query($sql) === TRUE) {
+    $last_id = $conn->insert_id;
+    echo "New record created successfully. Last inserted ID is: " . $last_id . "\r\n";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$sql = "SELECT id, firstname, lastname FROM myDB.MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "\r\nid: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
 ?>
     </body>
 </html>
